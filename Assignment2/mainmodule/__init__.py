ba06faefc83e317b60ce763ed974b67434267b56
Chris P. OConnell <chrispoconnell@outlook.com>
2015-10-07 19:40:18 -0400

    Pulling down

--- Assignment2/mainmodule/__init__.py
+++ Assignment2/mainmodule/__init__.py
@@ -1,3 +1,14 @@
 __author__ = 'chris'
-from mainmodule.menus import *
-mainmenu()
+from mainmodule.menus import mainmenu
+from mainmodule.menus import collectprovinces
+
+
+result=mainmenu()
+if result == '1':
+    collectprovinces()
+elif result == '2':
+    print("this function is not yet working.")
+elif result =='?':
+    print("this function is not yet working.")
+elif result =='Q':
+    print("Bye now, come back soon!.")
